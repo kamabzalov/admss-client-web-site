87caecc6764190e55c5e8bcf6697a1f997dbac40
Kamil Abzalov <mail@kamil-abzalov.com>
2024-07-30 21:22:31 +0300

    Change user

--- app/app-config.ts
+++ app/app-config.ts
@@ -1,2 +1,2 @@
 export const API_HOST = 'https://app.admss.com/api/v1/sites/';
-export const API_KEY = 'e29500772068fcfe7f3622636891781b';
+export const API_KEY = 'e0f4e8819c5513b682f33139c1bda076';
